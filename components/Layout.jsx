b4b89899b2aef4b6cfeb845f16b8f82d4f47d8a9
Sebastian Gomez <seagomezar@gmail.com>
2025-02-25 18:43:51 -0500

    feat(Analytics): :sparkles: Integrate Vercel Analytics into the Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import { Analytics } from "@vercel/analytics/react"
 
 function Layout({ children }) {
   return (
     <>
       <Header />
+      <Analytics/>
       {children}
     </>
   );
