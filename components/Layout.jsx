c727d44e968871795f676f4f6ccf91c28db8343f
Sebastian Gomez <seagomezar@gmail.com>
2025-02-25 19:22:15 -0500

    fix(Layout): :bug: Remove Analytics component from Layout to streamline rendering

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Analytics } from '@vercel/analytics/react';
 import Header from './Header';
 
 function Layout({ children }) {
   return (
     <>
-      <Analytics />
       <Header />
       {children}
     </>
